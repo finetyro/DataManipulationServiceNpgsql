7a6a9ea878a1ea8cf8b43bd92fe67b9b1cdfad7d
finetyro <finetyro@yandex.ru>
2024-08-25 13:52:03 +0300

    Adding read functional

--- DataManipulate.cs
+++ DataManipulate.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Common;
+﻿using System.Data.Common;
 using System.Data;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using Npgsql;
 using System.Transactions;
 using Newtonsoft.Json;
+using System.Dynamic;
+using System.Collections.Generic;
+using System.Linq;
+using System;
 
 namespace ru.finetyro.data.npgsql.manipulate
 {
@@ -15,6 +16,7 @@ namespace ru.finetyro.data.npgsql.manipulate
         public string ColumnName { get; set; }
         public object ColumnValue { get; set; }
         public DbType ColumnType { get; set; }
+        public string ColumnAlias { get; set; }
     }
 
     public class SpecificColumn
@@ -22,10 +24,23 @@ namespace ru.finetyro.data.npgsql.manipulate
         public string ColumnName { get; }
         public DbType ColumnType { get; }
         public SpecificDataSet DataSet { get; }
+        public string ColumnAlias { get; set; }
 
         public SpecificDataSet Set(object colValue)
         {
-            DataSet.GetMetaData().AddColumn(ColumnName, colValue);
+            DataSet.GetMetaData().AddColumn(ColumnName, colValue, ColumnAlias ?? ColumnName);
+            return DataSet;
+        }
+
+        public SpecificColumn As(string alias)
+        {
+            ColumnAlias = alias;
+            return this;
+        }
+
+        public SpecificDataSet Get()
+        {
+            DataSet.GetMetaData().AddColumn(ColumnName, null, ColumnAlias ?? ColumnName);
             return DataSet;
         }
 
@@ -73,13 +88,13 @@ namespace ru.finetyro.data.npgsql.manipulate
 
     public class DataSetMetaData
     {
-        public TransactionOptions _transactionOptions { get; set; }
+        public TransactionOptions TransactionOptions { get; set; }
 
         private bool InitializeTable;
         public bool IsTransaction { get; set; }
-        public TransactionScopeOption _tranScopeOption { get; set; }
-        public List<ColumnMetaData> _columns { get; }
-        public List<ColumnMetaData> _keys { get; }
+        public TransactionScopeOption TranScopeOption { get; set; }
+        public List<ColumnMetaData> Columns { get; }
+        public List<ColumnMetaData> Keys { get; }
         public string SchemaName { get; private set; }
         public string TableName { get; private set; }
 
@@ -96,44 +111,38 @@ namespace ru.finetyro.data.npgsql.manipulate
 
         public DataSetMetaData()
         {
-            _columns = new List<ColumnMetaData>();
-            _keys = new List<ColumnMetaData>();
+            Columns = new List<ColumnMetaData>();
+            Keys = new List<ColumnMetaData>();
             InitializeTable = false;
             IsTransaction = false;
-            _tranScopeOption = TransactionScopeOption.Required;
-            _transactionOptions = new TransactionOptions
+            TranScopeOption = TransactionScopeOption.Required;
+            TransactionOptions = new TransactionOptions
             {
                 IsolationLevel = System.Transactions.IsolationLevel.ReadCommitted,
                 Timeout = TimeSpan.FromSeconds(30)
             };
         }
 
-        public void AddColumn(string colName, object colValue)
+        public void AddColumn(string colName, object colValue, string alias)
         {
-            _columns.Add(new ColumnMetaData { ColumnName = colName, ColumnType = DbType.Object, ColumnValue = colValue });
-        }
-
-        public void AddColumn(string colName, DbType colType, object colValue)
-        {
-            _columns.Add(new ColumnMetaData { ColumnName = colName, ColumnType = colType, ColumnValue = colValue });
+            Columns.Add(new ColumnMetaData { ColumnName = colName, ColumnType = DbType.Object, ColumnValue = colValue, ColumnAlias = alias });
         }
 
         public void AddKey(string keyName, object keyValue)
         {
-            _keys.Add(new ColumnMetaData { ColumnName = keyName, ColumnType = DbType.Object, ColumnValue = keyValue });
+            Keys.Add(new ColumnMetaData { ColumnName = keyName, ColumnType = DbType.Object, ColumnValue = keyValue });
         }
 
         public void AddKey(string keyName, DbType keyType, object keyValue)
         {
-            _keys.Add(new ColumnMetaData { ColumnName = keyName, ColumnType = keyType, ColumnValue = keyValue });
+            Keys.Add(new ColumnMetaData { ColumnName = keyName, ColumnType = keyType, ColumnValue = keyValue });
         }
     }
 
     public class SpecificDataSet : IDisposable
     {
         private IDbConnection _dbConnection;
-        private readonly DataSetMetaData _metaData;
-
+        private DataSetMetaData _metaData;
 
         public DataSetMetaData GetMetaData()
         {
@@ -170,6 +179,7 @@ namespace ru.finetyro.data.npgsql.manipulate
 
         public SpecificDataSet Truncate()
         {
+            CheckExistsTableName();
             var sql = $"truncate table {_metaData.SchemaName}.{_metaData.TableName};";
             ExecuteSql(sql);
             return this;
@@ -177,9 +187,9 @@ namespace ru.finetyro.data.npgsql.manipulate
 
         private string GetKeysWhere()
         {
-            if (_metaData._keys.Any())
+            if (_metaData.Keys.Any())
             {
-                var keysCommands = _metaData._keys
+                var keysCommands = _metaData.Keys
                     .ToList()
                     .Select(it => $"{it.ColumnName} = :{it.ColumnName}_cnd_p");
                 var strKeys = string.Join(" and ", keysCommands);
@@ -191,7 +201,7 @@ namespace ru.finetyro.data.npgsql.manipulate
 
         private DbParameter[] GetKeysParameters()
         {
-            return _metaData._keys
+            return _metaData.Keys
                         .ToList()
                         .Select(it => new NpgsqlParameter { DbType = it.ColumnType, ParameterName = $"{it.ColumnName}_cnd_p", Value = it.ColumnValue })
                         .ToArray();
@@ -199,7 +209,7 @@ namespace ru.finetyro.data.npgsql.manipulate
 
         private DbParameter[] GetColsParameters()
         {
-            return _metaData._columns
+            return _metaData.Columns
                         .ToList()
                         .Select(it => new NpgsqlParameter { DbType = it.ColumnType, ParameterName = $"{it.ColumnName}_p", Value = it.ColumnValue })
                         .ToArray();
@@ -207,7 +217,7 @@ namespace ru.finetyro.data.npgsql.manipulate
 
         public string GetPreparedUpdateQuery()
         {
-            var cols = _metaData._columns.ToList();
+            var cols = _metaData.Columns.ToList();
             var colsCommands = cols.Select(it => $"{it.ColumnName} = :{it.ColumnName}_p");
             var strCols = string.Join(", ", colsCommands);
             var sql = $@"update {_metaData.SchemaName}.{_metaData.TableName} set {strCols}
@@ -249,8 +259,8 @@ namespace ru.finetyro.data.npgsql.manipulate
 
         public SpecificDataSet WithTransaction(System.Transactions.IsolationLevel isolationLevel, int timeoutSeconds, TransactionScopeOption transactionOption)
         {
-            _metaData._tranScopeOption = transactionOption;
-            _metaData._transactionOptions = new TransactionOptions
+            _metaData.TranScopeOption = transactionOption;
+            _metaData.TransactionOptions = new TransactionOptions
             {
                 IsolationLevel = isolationLevel,
                 Timeout = TimeSpan.FromSeconds(timeoutSeconds)
@@ -263,7 +273,7 @@ namespace ru.finetyro.data.npgsql.manipulate
         {
             SpecificDataSet Do()
             {
-                var cols = _metaData._columns.ToList();
+                var cols = _metaData.Columns.ToList();
                 var colsForInsert = cols.Select(it => it.ColumnName);
                 var strCols = string.Join(", ", colsForInsert);
                 var valuesForInsert = cols.Select(it => $":{it.ColumnName}_p");
@@ -275,7 +285,7 @@ namespace ru.finetyro.data.npgsql.manipulate
 
             if (_metaData.IsTransaction)
             {
-                using (TransactionScope scope = new TransactionScope(_metaData._tranScopeOption, _metaData._transactionOptions))
+                using (TransactionScope scope = new TransactionScope(_metaData.TranScopeOption, _metaData.TransactionOptions))
                 {
                     Do();
                 }
@@ -287,9 +297,10 @@ namespace ru.finetyro.data.npgsql.manipulate
 
         public SpecificDataSet Update()
         {
+            CheckExistsTableName();
             if (_metaData.IsTransaction)
             {
-                using (TransactionScope scope = new TransactionScope(_metaData._tranScopeOption, _metaData._transactionOptions))
+                using (TransactionScope scope = new TransactionScope(_metaData.TranScopeOption, _metaData.TransactionOptions))
                 {
                     ExecuteSql(GetPreparedUpdateQuery(), GetKeysParameters().Concat(GetColsParameters()).ToArray());
                 }
@@ -301,6 +312,7 @@ namespace ru.finetyro.data.npgsql.manipulate
 
         public SpecificDataSet Delete()
         {
+            CheckExistsTableName();
             SpecificDataSet Do()
             {
                 var sql = $"delete from {_metaData.SchemaName}.{_metaData.TableName} where {GetKeysWhere()}";
@@ -310,7 +322,7 @@ namespace ru.finetyro.data.npgsql.manipulate
 
             if (_metaData.IsTransaction)
             {
-                using (TransactionScope scope = new TransactionScope(_metaData._tranScopeOption, _metaData._transactionOptions))
+                using (TransactionScope scope = new TransactionScope(_metaData.TranScopeOption, _metaData.TransactionOptions))
                 {
                     Do();
                 }
@@ -336,6 +348,71 @@ namespace ru.finetyro.data.npgsql.manipulate
             }
         }
 
+        private List<dynamic> SqlRead(string sql, DbParameter[] parameters = null)
+        {
+            var colAliasDict = this._metaData.Columns
+                .ToDictionary(it => it.ColumnName, it => it.ColumnAlias);
+
+            List<dynamic> results = new List<dynamic>();
+            using (var command = new NpgsqlCommand(sql, (NpgsqlConnection)_dbConnection))
+            {
+                if (parameters != null)
+                {
+                    foreach (NpgsqlParameter parameter in parameters)
+                    {
+                        command.Parameters.Add(parameter);
+                    };
+                }
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        dynamic expando = new ExpandoObject();
+                        var expandoDict = (IDictionary<string, object>)expando;
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            var key = reader.GetName(i);
+                            string columnName = colAliasDict.ContainsKey(key) ? colAliasDict[key] : key;
+                            object value = reader.IsDBNull(i) ? null : reader.GetValue(i);
+                            expandoDict[columnName] = value;
+                        }
+                        results.Add(expando);
+                    }
+                }
+            }
+            return results;
+        }
+
+        private string GetColsForSelect()
+        {
+            var cols = _metaData.Columns.ToList();
+            var colsForSelect = cols.Select(it => it.ColumnName);
+            return string.Join(", ", colsForSelect);
+        }
+
+        public List<dynamic> Select()
+        {
+            CheckExistsTableName();
+            var sql = $@"select {GetColsForSelect()} from {_metaData.SchemaName}.{_metaData.TableName} 
+                         where {GetKeysWhere()}";
+            return SqlRead(sql, GetKeysParameters());
+        }
+
+        public List<dynamic> Select(string query, DbParameter[] parameters = null)
+        {
+            CheckExistsTableName();
+            if (_metaData.Columns.Any()) query = $"select {GetColsForSelect()} from ({query}) d";
+            if (_metaData.Keys.Any()) query = string.Concat(query, $" where {GetKeysWhere()} ");
+            return SqlRead(query, parameters ?? new NpgsqlParameter[] { }.Concat(GetKeysParameters()).ToArray());
+        }
+
+        public SpecificDataSet ClearMetaData()
+        {
+            _metaData = new DataSetMetaData();
+            return this;
+        }
+
         public override string ToString()
         {
             return JsonConvert.SerializeObject(_metaData, Newtonsoft.Json.Formatting.Indented);
@@ -344,6 +421,12 @@ namespace ru.finetyro.data.npgsql.manipulate
         public void Dispose()
         {
             _dbConnection.Dispose();
+        }
+
+        private void CheckExistsTableName()
+        {
+            if (_metaData == null) throw new Exception("Missing meta data!");
+            if (_metaData.TableName == null) throw new Exception("Missing table name!");
         }
     }
 }
--- Example.cs
+++ Example.cs
@@ -1,46 +1,75 @@
 ﻿using Npgsql;
 using ru.finetyro.data.npgsql.manipulate;
 using System;
+using System.Linq;
 using System.Transactions;
 
 namespace NpgsqlMiniDataManipulationService
 {
     internal class Example
     {
+        private const string TEST = "test";
+        private const string PUBLIC = "public";
+        private const string CARDNUMBER = "card_number";
+        private const string LASTNAME = "last_name";
+        private const string COMMENT = "comment";
+        private const string CONNECTIONSTRING = "your connection string =)";
+        
         void DoExample()
         {
-            NpgsqlConnection connection = new NpgsqlConnection("your connection string =)");
+            NpgsqlConnection connection = new NpgsqlConnection(CONNECTIONSTRING);
 
             connection.Open();
             new SpecificDataSet(connection, true)
-                .Table("test")
+                .Table(TEST)
                 .Truncate();
-
-            //Insert example
+            
+            //The insert example
             new SpecificDataSet(connection, true)
-                .Table("test")
+                .Table(TEST)
                     .Column("first_name", System.Data.DbType.String).Set("Mr. Ivanov")
-                    .Column("last_name").Set("Ivan")
+                    .Column(LASTNAME).Set("Ivan")
                     .Column("birth_date").Set(new DateTime(2000, 7, 20))
-                    .Column("comment").Set("the diligent student")
+                    .Column(COMMENT).Set("the diligent student")
                     .WithTransaction(IsolationLevel.ReadCommitted, 10, TransactionScopeOption.Required)
                 .Insert();
 
-            //Update example
+            //The update example
             new SpecificDataSet()
                 .For(connection)
-                .Table("test")
-                    .Column("card_number").Set(10)
-                    .Column("comment").Set("the diligent student #10")
+                .Table(TEST)
+                    .Column(CARDNUMBER).Set(10)
+                    .Column(COMMENT).Set("the diligent student #10")
                     .WithTransaction(IsolationLevel.ReadCommitted, 10, TransactionScopeOption.Required)
-                .WithKeys("card_number").Set(1)
+                .WithKeys(CARDNUMBER).Set(1)
                 .Update();
+            
+            //The select example
+            var students = new SpecificDataSet(connection, true)
+                .Table(PUBLIC, TEST)
+                    .Column(CARDNUMBER).As("CID").Get()
+                    .Column(LASTNAME).As("LName").Get()
+                .WithKeys(CARDNUMBER).Set(10)
+                .Select();
+            
+            var queriedStudents = from student in students
+                                select new
+                                {
+                                    student.CID,
+                                    student.LName
+                                };
 
-            //Delete example
+            //The select example with query
+            var studentsWithQuery = new SpecificDataSet(connection, true)
+                .Table(PUBLIC, TEST)
+                    .Column(LASTNAME).As("LName").Get()
+                .Select("select * from test");
+
+            //The delete example
             new SpecificDataSet(connection, true)
-                .Table("public", "test")
+                .Table(PUBLIC, TEST)
                     .WithTransaction(IsolationLevel.Serializable, 10, TransactionScopeOption.Required)
-                .WithKeys("card_number").Set(10)
+                .WithKeys(CARDNUMBER).Set(10)
                 .Delete()
                 .Dispose();
         }
